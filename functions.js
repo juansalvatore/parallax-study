a3ddace846c637cd79750609a755decae75f5eb9
Juan Salvatore <juansalvatore@live.com.ar>
2017-08-23 22:24:11 -0300

    I created a Banner with Parallax

---
+++ functions.js
@@ -0,0 +1,17 @@
+
+$(window).scroll(function () {
+  var wScroll = $(this).scrollTop()
+  console.log(wScroll)
+
+  $('.logo').css({
+    'transform' : 'translate(0px, '+ wScroll/2 + '%)'
+  })
+
+  $('.back-bird').css({
+    'transform' : 'translate(0px, '+ wScroll/4 + '%)'
+  })
+
+  $('.fore-bird').css({
+    'transform' : 'translate(0px, -'+ wScroll/45 + '%)'
+  })
+})
